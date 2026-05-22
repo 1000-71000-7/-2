7a58f705eba8a1319bfe58633ca2a5d6071d6dfb
1000-71000-7 <92521333+1000-71000-7@users.noreply.github.com>
2026-05-22 18:04:15 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+# Добавляем текущую директорию в путь (на всякий случай)
+sys.path.insert(0, os.path.dirname(__file__))
+
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
